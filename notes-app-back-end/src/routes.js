ea7534d9e65f0c99cbdbd4ec187505d74c54aa55
ferzian <m.ferzian09@gmail.com>
2024-10-24 09:28:39 +0700

    Edit Note

--- notes-app-back-end/src/routes.js
+++ notes-app-back-end/src/routes.js
@@ -1,16 +1,26 @@
-const {addNoteHandler} = require('./handler');
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler } = require("./handler");
 
 const routes = [
-    {
-        method: 'POST',
-        path: '/notes',
-        handler: addNoteHandler,
-        options: {
-          cors: {
-            origin: ['*'],
-          },
-        },
-      },
+  {
+    method: "POST",
+    path: "/notes",
+    handler: addNoteHandler,
+  },
+  {
+    method: "GET",
+    path: "/notes",
+    handler: getAllNotesHandler,
+  },
+  {
+    method: "GET",
+    path: "/notes/{id}",
+    handler: getNoteByIdHandler,
+  },
+  {
+    method: "PUT",
+    path: "/notes/{id}",
+    handler: editNoteByIdHandler,
+  },
 ];
 
 module.exports = routes;
